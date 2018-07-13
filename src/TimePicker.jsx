6f2691bf7c61e5ce52f116ce75b92e95337818ad
justin.jin <justin.jin@grapecity.com>
2018-07-13 18:36:28 +0800

    Support customize popup style

--- src/TimePicker.jsx
+++ src/TimePicker.jsx
@@ -36,6 +36,7 @@ export default class Picker extends Component {
     style: PropTypes.object,
     className: PropTypes.string,
     popupClassName: PropTypes.string,
+    popupStyle: PropTypes.object,
     disabledHours: PropTypes.func,
     disabledMinutes: PropTypes.func,
     disabledSeconds: PropTypes.func,
@@ -69,6 +70,7 @@ export default class Picker extends Component {
     style: {},
     className: '',
     popupClassName: '',
+    popupStyle: {},
     id: '',
     align: {},
     defaultOpenValue: moment(),
@@ -268,7 +270,7 @@ export default class Picker extends Component {
     const {
       prefixCls, placeholder, placement, align, id,
       disabled, transitionName, style, className, getPopupContainer, name, autoComplete,
-      onFocus, onBlur, autoFocus, inputReadOnly, inputIcon,
+      onFocus, onBlur, autoFocus, inputReadOnly, inputIcon, popupStyle,
     } = this.props;
     const { open, value } = this.state;
     const popupClassName = this.getPopupClassName();
@@ -276,6 +278,7 @@ export default class Picker extends Component {
       <Trigger
         prefixCls={`${prefixCls}-panel`}
         popupClassName={popupClassName}
+        popupStyle={popupStyle}
         popup={this.getPanelElement()}
         popupAlign={align}
         builtinPlacements={placements}
