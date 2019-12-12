4f96a5a7506e11210685ffec51eefd5a6a2b0831
Justin Nazari <justinnazari@gmail.com>
2019-12-12 11:38:09 -0800

    Add lib/es files

---
+++ es/Select.js
@@ -0,0 +1,197 @@
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+/* eslint jsx-a11y/no-noninteractive-element-to-interactive-role: 0 */
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import raf from 'raf';
+
+var scrollTo = function scrollTo(element, to, duration) {
+  // jump to target if duration zero
+  if (duration <= 0) {
+    raf(function () {
+      // eslint-disable-next-line no-param-reassign
+      element.scrollTop = to;
+    });
+    return;
+  }
+
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+  raf(function () {
+    // eslint-disable-next-line no-param-reassign
+    element.scrollTop += perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  });
+};
+
+var Select =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Select, _Component);
+
+  function Select() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, Select);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Select)).call.apply(_getPrototypeOf2, [this].concat(args)));
+    _this.state = {
+      active: false
+    };
+
+    _this.onSelect = function (value) {
+      var _this$props = _this.props,
+          onSelect = _this$props.onSelect,
+          type = _this$props.type;
+      onSelect(type, value);
+    };
+
+    _this.handleMouseEnter = function (e) {
+      var onMouseEnter = _this.props.onMouseEnter;
+
+      _this.setState({
+        active: true
+      });
+
+      onMouseEnter(e);
+    };
+
+    _this.handleMouseLeave = function () {
+      _this.setState({
+        active: false
+      });
+    };
+
+    _this.saveRoot = function (node) {
+      _this.root = node;
+    };
+
+    _this.saveList = function (node) {
+      _this.list = node;
+    };
+
+    return _this;
+  }
+
+  _createClass(Select, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      // jump to selected option
+      this.scrollToSelected(0);
+    }
+  }, {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate(prevProps) {
+      var selectedIndex = this.props.selectedIndex; // smooth scroll to selected option
+
+      if (prevProps.selectedIndex !== selectedIndex) {
+        this.scrollToSelected(120);
+      }
+    }
+  }, {
+    key: "getOptions",
+    value: function getOptions() {
+      var _this2 = this;
+
+      var _this$props2 = this.props,
+          options = _this$props2.options,
+          selectedIndex = _this$props2.selectedIndex,
+          prefixCls = _this$props2.prefixCls,
+          onEsc = _this$props2.onEsc;
+      return options.map(function (item, index) {
+        var _classNames;
+
+        var cls = classNames((_classNames = {}, _defineProperty(_classNames, "".concat(prefixCls, "-select-option-selected"), selectedIndex === index), _defineProperty(_classNames, "".concat(prefixCls, "-select-option-disabled"), item.disabled), _classNames));
+        var onClick = item.disabled ? undefined : function () {
+          _this2.onSelect(item.value);
+        };
+
+        var onKeyDown = function onKeyDown(e) {
+          if (e.keyCode === 13) onClick();else if (e.keyCode === 27) onEsc();
+        };
+
+        return React.createElement("li", {
+          role: "button",
+          onClick: onClick,
+          className: cls,
+          key: index // eslint-disable-line react/no-array-index-key
+          ,
+          disabled: item.disabled,
+          tabIndex: "0",
+          onKeyDown: onKeyDown
+        }, item.value);
+      });
+    }
+  }, {
+    key: "scrollToSelected",
+    value: function scrollToSelected(duration) {
+      // move to selected item
+      var selectedIndex = this.props.selectedIndex;
+
+      if (!this.list) {
+        return;
+      }
+
+      var index = selectedIndex;
+
+      if (index < 0) {
+        index = 0;
+      }
+
+      var topOption = this.list.children[index];
+      var to = topOption.offsetTop;
+      scrollTo(this.root, to, duration);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props3 = this.props,
+          prefixCls = _this$props3.prefixCls,
+          options = _this$props3.options;
+      var active = this.state.active;
+
+      if (options.length === 0) {
+        return null;
+      }
+
+      var cls = classNames("".concat(prefixCls, "-select"), _defineProperty({}, "".concat(prefixCls, "-select-active"), active));
+      return React.createElement("div", {
+        className: cls,
+        onMouseEnter: this.handleMouseEnter,
+        onMouseLeave: this.handleMouseLeave,
+        ref: this.saveRoot
+      }, React.createElement("ul", {
+        ref: this.saveList
+      }, this.getOptions()));
+    }
+  }]);
+
+  return Select;
+}(Component);
+
+export default Select;
--- es/placements.js
+++ es/placements.js
@@ -1,47 +1,44 @@
-const autoAdjustOverflow = {
+var autoAdjustOverflow = {
   adjustX: 1,
-  adjustY: 1,
+  adjustY: 1
 };
-
-const targetOffset = [0, 0];
-
-const placements = {
+var targetOffset = [0, 0];
+var placements = {
   bottomLeft: {
     points: ['tl', 'tl'],
     overflow: autoAdjustOverflow,
     offset: [0, -3],
-    targetOffset,
+    targetOffset: targetOffset
   },
   bottomRight: {
     points: ['tr', 'tr'],
     overflow: autoAdjustOverflow,
     offset: [0, -3],
-    targetOffset,
+    targetOffset: targetOffset
   },
   topRight: {
     points: ['br', 'br'],
     overflow: autoAdjustOverflow,
     offset: [0, 3],
-    targetOffset,
+    targetOffset: targetOffset
   },
   topLeft: {
     points: ['bl', 'bl'],
     overflow: autoAdjustOverflow,
     offset: [0, 3],
-    targetOffset,
+    targetOffset: targetOffset
   },
   customNormal: {
-    points: ['tl', 'tl'],
+    points: ['bl', 'bl'],
     overflow: autoAdjustOverflow,
     offset: [0, 26],
-    targetOffset,
+    targetOffset: targetOffset
   },
   customShort: {
-    points: ['tl', 'tl'],
+    points: ['bl', 'bl'],
     overflow: autoAdjustOverflow,
     offset: [0, 40],
-    targetOffset,
-  },
+    targetOffset: targetOffset
+  }
 };
-
 export default placements;
