9b51e1753b6642a36bf0b9e77b4c408c86a806d3
Georgi Kolev <georgi.lyubomirov.kolev@gmail.com>
2020-05-28 00:59:13 +0300

    update optional props
    edit onFocus and onBlur to be optional

--- index.d.ts
+++ index.d.ts
@@ -33,8 +33,8 @@ declare module "rc-time-picker" {
     transitionName?: string;
     name?: string;
     autoComplete?: string;
-    onFocus: (event: React.FocusEvent<HTMLInputElement>) => void;
-    onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+    onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
     onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
     autoFocus?: boolean;
     onOpen?: (newState: { open: true }) => void;
